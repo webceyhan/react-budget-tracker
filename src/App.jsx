303314dd5008645f0b6735333378ed21dab99591
Evren Ceyhan <evren@ceyhan.io>
2022-04-29 16:39:46 +0300

    app layout impl.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { Grid } from '@mui/material';
 import { Details } from './components/Details.jsx';
 
 export const App = () => {
     return (
         <div>
-            <Details />
+            <Grid
+                container
+                spacing={0}
+                alignItems="center"
+                justifyContent="center"
+                style={{ height: '100vh' }}
+            >
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                    main
+                </Grid>
+
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+            </Grid>
         </div>
     );
 };
