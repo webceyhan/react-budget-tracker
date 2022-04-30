deb8cc303a41d4c90cc31812788c1132879eb42b
Evren Ceyhan <evren@ceyhan.io>
2022-04-30 21:44:53 +0300

    theme removed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
 
 import { App } from './App';
-import { theme } from './theme';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <App />
-        </ThemeProvider>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <App />
     </React.StrictMode>
 );
