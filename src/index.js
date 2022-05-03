1ff7a9e7ccc0f6d46fd91c0137ffcfb75d968d2d
Evren Ceyhan <evren@ceyhan.io>
2022-05-04 01:49:50 +0300

    speech provider added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
+import { SpeechProvider } from '@speechly/react-client';
 
 import { App } from './App';
 import { TransactionContextProvider } from './context/transaction';
@@ -11,8 +12,10 @@ root.render(
     <React.StrictMode>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <TransactionContextProvider>
-            <App />
-        </TransactionContextProvider>
+        <SpeechProvider appId='ad906d44-2cd0-4063-ab7f-111c095ac8e3'>
+            <TransactionContextProvider>
+                <App />
+            </TransactionContextProvider>
+        </SpeechProvider>
     </React.StrictMode>
 );
