eac24b8e6fce6a981b500f69332853dbb747b83f
Evren Ceyhan <evren@ceyhan.io>
2022-05-05 22:33:44 +0300

    autoscroll to top on listening impl.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import {
     PushToTalkButton,
     PushToTalkButtonContainer,
 } from '@speechly/react-ui';
+import { useEffect, useRef } from 'react';
+import { useSpeechContext, ClientState } from '@speechly/react-client';
 
 // response grid styles
 const sxGrid1 = { order: { xs: 2, sm: 1 } };
@@ -15,6 +17,19 @@ const sxGrid2 = { order: { xs: 1, sm: 2 } };
 const sxGrid3 = { order: { xs: 3, sm: 3 } };
 
 export const App = () => {
+    const mainRef = useRef()
+    const { clientState } = useSpeechContext();
+
+    const handleScroll = () =>
+        mainRef.current.scrollIntoView({ behavior: 'smooth' });
+
+    useEffect(() => {
+        // check if user speaking and scroll up
+        if (clientState === ClientState.Recording) {
+            handleScroll();
+        }
+    }, [clientState]);
+
     return (
         <Container>
             <Grid
@@ -28,8 +43,8 @@ export const App = () => {
                     <Details title="Income" />
                 </Grid>
 
-                <Grid item xs={12} sm={4} sx={sxGrid2}>
-                    <Main />
+                <Grid item xs={12} sm={4} sx={sxGrid2} ref={mainRef}>
+                    <Main  />
                 </Grid>
 
                 <Grid item xs={12} sm={4} sx={sxGrid3}>
