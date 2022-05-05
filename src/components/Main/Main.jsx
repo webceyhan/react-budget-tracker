c6284f80096433e8c79730e44b80cc8306324b86
Evren Ceyhan <evren@ceyhan.io>
2022-05-05 21:03:34 +0300

    main comp restyled

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -19,19 +19,19 @@ export const Main = () => {
                 title="Expense Tracker"
                 subheader="Powered by Speechly"
             />
-            <CardContent>
-                <Typography align="center" variant="h5">
+            <CardContent sx={{ py: 0 }}>
+                <Typography align="center" variant="h6">
                     Total Balance ${balance}
                 </Typography>
-                <Typography variant="subtitle1" mt={2}>
+                <Typography variant="body2" mt={2} color="GrayText">
                     {/* InfoCard */}
-                    try saying: add income for $100 in category salary for
+                    try saying: Add income for $100 in category salary for
                     monday ...
                 </Typography>
                 <Divider sx={{ my: 2 }} />
                 <Form />
             </CardContent>
-            <CardContent sx={{ pt: 5 }}>
+            <CardContent sx={{ py: 0 }}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
                         <List />
